c98a6935fea1e3f69485d47c99d65f1a79db2f50
Pierre Lardet <lardet.pierre@gmail.com>
2023-02-08 10:52:49 +0000

    init2

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+    entry: [
+        './src/client.ts',
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /node_modules/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            esModule: false,
+                        },
+                    },
+                    'css-loader',
+                    'resolve-url-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|json)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            minify: false
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'main.[contenthash].css',
+        }),
+        // new CopyPlugin({
+        //     patterns: [
+        //         {
+        //             from: './src/client/public', to: './', globOptions: {
+        //                 ignore: ['**/assets/**', '**/css/**', '**.html']
+        //             },
+        //             noErrorOnMissing: true
+        //         }
+        //     ],
+        // }),
+    ],
+    devtool: 'source-map',
+    output: {
+        filename: 'main.[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: 'assets/[name].[contenthash][ext][query]',
+        clean: true
+    },
+    watchOptions: {
+        ignored: /node_modules/,
+    },
+    devServer: {
+        static: './src/client/public',
+        port: 8080,
+        host: '0.0.0.0',
+    },
+}
