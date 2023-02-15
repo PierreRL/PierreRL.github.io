3e4fb3fc6bc7827a0e44e3d98c3a52184dd5fd6b
Pierre Lardet <lardet.pierre@gmail.com>
2023-02-15 12:51:08 +0000

    architecture

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import './assets/styles/main-style.scss'
 import './assets/styles/nav-footer.scss'
-import { start } from './threeJS/three-manager';
+import { start } from './graphics/three-manager';
 
 start()
