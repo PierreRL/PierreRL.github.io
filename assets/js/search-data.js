9e9ad85e8a0fd7f4ea1703eacc2655b2956624a8
PierreRL <43621803+PierreRL@users.noreply.github.com>
2024-12-29 21:48:12 +0000

    Deploying to gh-pages from @ PierreRL/PierreRL.github.io@cd4ecb0fe9124603de64533178d195862ce6b4ee 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,13 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-cv",
-          title: "CV",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
         },{id: "post-a-post-with-image-galleries",
       
         title: "a post with image galleries",
